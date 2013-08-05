38b4166de87a5e564a468f69cec3d04cf02a6b8f
d1ffuz0r <d1fffuz0r@gmail.com>
2013-08-05 21:40:39 +0700

    changes by commentaries

--- static/js/main.js
+++ static/js/main.js
@@ -4,13 +4,13 @@ var SUPPORT = 1;
 var DD = 2;
 var SHIT = 3;
 var professions = {
-    1: {'pid': 1, 'type': SHIT, 'title': 'Кладоискатель (Fortune Seeker)', 'chance': 7.42, 'img': './img/prof/.png'},
+    1: {'pid': 1, 'type': SHIT, 'title': 'Кладоискатель (Fortune Seeker)', 'chance': 6.77, 'img': './img/prof/.png'},
     2: {'pid': 2, 'type': SUPPORT, 'title': 'Глас Судьбы (Doom Cryer)', 'chance': 6.11, 'img': './img/prof/.png'},
     3: {'pid': 3, 'type': DD, 'title': 'Каратель (Doombringer)', 'chance': 6.09, 'img': './img/prof/.png'},
     4: {'pid': 4, 'type': DD, 'title': 'Титан (Titan)', 'chance': 5.34, 'img': './img/prof/.png'},
     5: {'pid': 5, 'type': DD, 'title': 'Повелитель Бури (Storm Screamer)', 'chance': 4.69, 'img': './img/prof/.png'},
     6: {'pid': 6, 'type': SUPPORT, 'title': 'Призрачный Танцор (Spectral Dancer)', 'chance': 4.62, 'img': './img/prof/.png'},
-    7: {'pid': 7, 'type': DD, 'title': 'Мастер (Maestro)', 'chance': 4.53, 'img': './img/prof/.png'},
+    7: {'pid': 7, 'type': SHIT, 'title': 'Мастер (Maestro)', 'chance': 4.53, 'img': './img/prof/.png'},
     8: {'pid': 8, 'type': SUPPORT, 'title': 'Жрец Евы (Evas Saint)', 'chance': 4.26, 'img': './img/prof/.png'},
     9: {'pid': 9, 'type': DD, 'title': 'Авантюрист (Adventurer)', 'chance': 3.90, 'img': './img/prof/.png'},
     10: {'pid': 10, 'type': SUPPORT, 'title': 'Деспот (Dominator)', 'chance': 3.60, 'img': './img/prof/.png'},
@@ -21,14 +21,14 @@ var professions = {
     15: {'pid': 15, 'type': DD, 'title': 'Снайпер (Sagittarius)', 'chance': 2.81, 'img': './img/prof/.png'},
     16: {'pid': 16, 'type': DD, 'title': 'Призрачный Охотник (Ghost Hunter)', 'chance': 2.61, 'img': './img/prof/.png'},
     17: {'pid': 17, 'type': SUPPORT, 'title': 'Жрец Шилен (Shillien Saint)', 'chance': 2.57, 'img': './img/prof/.png'},
-    18: {'pid': 18, 'type': DD, 'title': 'Храмовник Шилен (Shillien Templar)', 'chance': 2.41, 'img': './img/prof/.png'},
-    19: {'pid': 19, 'type': DD, 'title': 'Рыцарь Феникса (Phoenix Knight)', 'chance': 2.32, 'img': './img/prof/.png'},
+    18: {'pid': 18, 'type': SHIT, 'title': 'Храмовник Шилен (Shillien Templar)', 'chance': 2.41, 'img': './img/prof/.png'},
+    19: {'pid': 19, 'type': SHIT, 'title': 'Рыцарь Феникса (Phoenix Knight)', 'chance': 2.32, 'img': './img/prof/.png'},
     20: {'pid': 20, 'type': DD, 'title': 'Пожиратель Душ (Soultaker)', 'chance': 2.20, 'img': './img/prof/.png'},
     21: {'pid': 21, 'type': SUPPORT, 'title': 'Виртуоз (Sword Muse)', 'chance': 2.14, 'img': './img/prof/.png'},
-    22: {'pid': 22, 'type': DD, 'title': 'Рыцарь Ада (Hell Knight)', 'chance': 1.91, 'img': './img/prof/.png'},
+    22: {'pid': 22, 'type': SHIT, 'title': 'Рыцарь Ада (Hell Knight)', 'chance': 1.91, 'img': './img/prof/.png'},
     23: {'pid': 23, 'type': DD, 'title': 'Магистр Магии (Mystic Muse)', 'chance': 1.88, 'img': './img/prof/.png'},
-    24: {'pid': 24, 'type': DD, 'title': 'Чернокнижник (Arcana Lord)', 'chance': 1.87, 'img': './img/prof/.png'},
-    25: {'pid': 25, 'type': SUPPORT, 'title': 'Апостол (Hierophant)', 'chance': 1.65, 'img': './img/prof/.png'},
+    24: {'pid': 24, 'type': SHIT, 'title': 'Чернокнижник (Arcana Lord)', 'chance': 1.87, 'img': './img/prof/.png'},
+    25: {'pid': 25, 'type': SHIT, 'title': 'Апостол (Hierophant)', 'chance': 1.65, 'img': './img/prof/.png'},
     26: {'pid': 26, 'type': DD, 'title': 'Полководец (Dreadnought)', 'chance': 1.64, 'img': './img/prof/.png'},
     27: {'pid': 27, 'type': DD, 'title': 'Страж Теней (Ghost Sentinel)', 'chance': 1.54, 'img': './img/prof/.png'},
     28: {'pid': 28, 'type': DD, 'title': 'Страж Лунного Света (Moonlight Sentinel)', 'chance': 1.34, 'img': './img/prof/.png'},
@@ -38,25 +38,26 @@ var professions = {
     32: {'pid': 32, 'type': DD, 'title': 'Диверсант (Trickster)', 'chance': 1.06, 'img': './img/prof/.png'},
     33: {'pid': 33, 'type': DD, 'title': 'Инквизитор (Male Soul Hound)', 'chance': 1.01, 'img': './img/prof/.png'},
     34: {'pid': 34, 'type': DD, 'title': 'Странник Ветра (Wind Rider)', 'chance': 0.98, 'img': './img/prof/.png'},
-    35: {'pid': 35, 'type': DD, 'title': 'Храмовник Евы (Evas Templar)', 'chance': 0.77, 'img': './img/prof/.png'},
+    35: {'pid': 35, 'type': SHIT, 'title': 'Храмовник Евы (Evas Templar)', 'chance': 0.77, 'img': './img/prof/.png'},
+    36: {'pid': 36, 'type': SHIT, 'title': 'Арбитр (Judicator)', 'chance': 0.65, 'img': './img/prof/.png'},
 };
 
 var profs_count = Object.keys(professions).length;
 var global_counter = 1;
 var selected = 0;
 
-var select_prof = function(){
+var select_prof = function() {
     var prof = null;
 
-    while(!prof){
-        prof = professions[Math.floor((Math.random(profs_count) * profs_count))];
+    while(!prof) {
+        prof = professions[Math.floor((Math.random() * profs_count) + 1)];
     }
     return prof;
 
 };
 
-var contains = function(list, key){
-    return list.filter(function(x){ return x.get('pid') == key }).length;
+var contains = function(list, key) {
+    return list.filter(function(x) { return x.get('pid') == key }).length;
 };
 
 
@@ -73,11 +74,11 @@ var Game = Backbone.Model.extend({});
 // collections
 var CardsCollection = Backbone.Collection.extend({
     model: Card,
-    addCard: function(card){
+    addCard: function(card) {
         this.container.append(card.render().el);
         this.add(card.model)
     },
-    removeCard: function(prof){
+    removeCard: function(prof) {
         this.container.find('#card' + prof).remove()
         this.remove(prof);
     }
@@ -90,8 +91,8 @@ var ShitCollection = CardsCollection.extend({});
 
 // views
 var BaseView = Backbone.View.extend({
-    hide: function(){ this.$el.removeClass('active'); },
-    show: function(){ this.$el.addClass('active'); },
+    hide: function() { this.$el.removeClass('active'); },
+    show: function() { this.$el.addClass('active'); },
 });
 
 var MainView = BaseView.extend({el: $("#main-window")});
@@ -106,7 +107,7 @@ var CardView = BaseView.extend({
   className: 'cart-container',
   template: _.template(_.unescape($('.cart-container').html())),
 
-  initialize: function(){
+  initialize: function() {
         this.model.bind('change', this.render, this);
         this.model.bind('destroy', this.remove, this);
   },
@@ -138,7 +139,7 @@ var GameView = BaseView.extend({
         'click .pass-party': 'pass_party',
     },
 
-    get_collection_by_type: function(type){
+    get_collection_by_type: function(type) {
         switch(type) {
             case 1:
                 collection = this.support_collection;
@@ -153,7 +154,7 @@ var GameView = BaseView.extend({
         return collection;
     },
 
-    create_card: function(id, item){
+    create_card: function(id, item ) {
         return new CardView({
             id: 'card' + id,
             model: new Card({
@@ -166,16 +167,16 @@ var GameView = BaseView.extend({
         });
     },
 
-    delete_seleted: function(collection){
-        collection.filter(function(i){ return i.get('clicked') }).map(
+    delete_seleted: function(collection) {
+        collection.filter(function(i) { return i.get('clicked') }).map(
             function(item) { collection.removeCard(item.id) }
         );
     },
 
-    update_collection: function(collection, end, replace){
+    update_collection: function(collection, end, replace) {
         if (!end) { end = 0; }
 
-        if (replace){ // replace items
+        if (replace) { // replace items
             this.delete_seleted(this.support_collection);
             this.delete_seleted(this.dd_collection);
             this.delete_seleted(this.shit_collection);
@@ -186,8 +187,8 @@ var GameView = BaseView.extend({
             this.push_to_collection(global_counter++, prof);
         }
     },
-    push_to_collection: function(id, item){
-        if(item.type == SUPPORT & this.support_collection.length <= 5){
+    push_to_collection: function(id, item) {
+        if(item.type == SUPPORT & this.support_collection.length <= 7) {
             if(!contains(this.support_collection, item.pid)) {
                 collection = this.support_collection;
             } else {
@@ -202,7 +203,7 @@ var GameView = BaseView.extend({
         collection.addCard(this.create_card(id, item));
     },
 
-    initialize: function(){
+    initialize: function() {
         this.support_collection = new SupportCollection();
         this.support_collection.container = this.$el.find('.support-row ul');
 
@@ -213,30 +214,29 @@ var GameView = BaseView.extend({
         this.shit_collection.container = this.$el.find('.shit-row ul');
 
         var profs = [];
-        for(var i = 1; i < 13; i++){ profs.push(select_prof());}
-        profs = _.sortBy(profs, function(i){ return i.type });
+        for(var i = 1; i < 13; i++) { profs.push(select_prof()); }
+        profs = _.sortBy(profs, function(i) { return i.type });
 
-        for(prof in profs){ this.push_to_collection(global_counter++, profs[prof]);}
+        for(prof in profs) { this.push_to_collection(global_counter++, profs[prof]); }
     },
 
-    pass_cards: function(){
-        if (selected == 2) {
+    pass_cards: function() {
+        if (selected == 3) {
             this.passes++;
 
-            if(this.passes <= 2){
-                this.update_collection(null, 2, true);
+            if(this.passes <= 2) {
+                this.update_collection(null, 3, true);
                 selected = 0;
-                if(this.passes == 2){
+                if(this.passes == 2) {
                     this.$el.find('.pass-cards').hide();
-                    this.$el.find('.pass-cards-message').show();
                 }
             }
         } else {
-            alert('Выберите 2 карты');
+            alert('Выберите 3 карты');
         }
     },
 
-    pass_party: function(){
+    pass_party: function() {
         if (selected == 3) {
             this.update_collection(null, 3, true);
             selected = 0;
@@ -247,9 +247,9 @@ var GameView = BaseView.extend({
         }
     },
 
-    check: function(){
+    check: function() {
         game_view.hide();
-        if(Math.round(Math.random(1)) == 1){
+        if(Math.round(Math.random(1)) == 1) {
             new WinView().show();
         } else {
             new FailView().show();
@@ -270,24 +270,21 @@ var App = Backbone.Router.extend({
         "!/fail":  "fail",
         "!/game":  "game",
     },
-
-    win: function(){
+    win: function() {
         new WinView().show();
     },
-    fail: function(){
+    fail: function() {
         new FailView().show();
     },
     start: function() {
         main_view.hide();
         start_view.show();
     },
-
     game: function() {
         main_view.hide();
         start_view.hide();
         game_view.show();
     },
-
     default: function() {
         main_view.show();
         start_view.hide();
@@ -296,7 +293,7 @@ var App = Backbone.Router.extend({
 });
 
 
-$(document).ready(function(){
+$(document).ready(function() {
     app = new App();
     Backbone.history.start();
 });
